132b035ac480173c94642b246823111cd86ebb24
Kamil Kisiel <kamil@kamilkisiel.net>
2013-10-10 15:29:44 -0700

    Cleanup in h5f.go

--- h5f.go
+++ h5f.go
@@ -13,42 +13,23 @@ import (
 	"unsafe"
 )
 
-// -------- The H5F API for accessing HDF5 files. ---------
-
 // File constants
 const (
-
-	// absence of rdwr => rd-only
-	F_ACC_RDONLY int = 0x0000
-
-	// open for read and write
-	F_ACC_RDWR int = 0x0001
-
-	// Truncate file, if it already exists, erasing all data previously stored in the file.
-	F_ACC_TRUNC int = 0x0002
-
-	// Fail if file already exists.
-	F_ACC_EXCL int = 0x0004
-
-	// print debug info
-	F_ACC_DEBUG int = 0x0008
-
-	// create non-existing files
-	F_ACC_CREAT int = 0x0010
-
-	// value passed to set_elink_acc_flags to cause flags to be taken from the parent file
-	F_ACC_DEFAULT int = 0xffff
+	F_ACC_RDONLY  int = 0x0000 // absence of rdwr => rd-only
+	F_ACC_RDWR    int = 0x0001 // open for read and write
+	F_ACC_TRUNC   int = 0x0002 // Truncate file, if it already exists, erasing all data previously stored in the file.
+	F_ACC_EXCL    int = 0x0004 // Fail if file already exists.
+	F_ACC_DEBUG   int = 0x0008 // print debug info
+	F_ACC_CREAT   int = 0x0010 // create non-existing files
+	F_ACC_DEFAULT int = 0xffff // value passed to set_elink_acc_flags to cause flags to be taken from the parent file
 )
 
 // The difference between a single file and a set of mounted files.
 type Scope C.H5F_scope_t
 
 const (
-	// specified file handle only.
-	F_SCOPE_LOCAL Scope = 0
-
-	// entire virtual file.
-	F_SCOPE_GLOBAL Scope = 1
+	F_SCOPE_LOCAL  Scope = 0 // specified file handle only.
+	F_SCOPE_GLOBAL Scope = 1 // entire virtual file.
 )
 
 // a HDF5 file
