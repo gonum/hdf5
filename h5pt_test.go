d40b6209084b1b469ad7c7cd50ea3f42ca892ecb
Sebastien Binet <binet@cern.ch>
2014-09-25 12:19:40 +0200

    test: enable test of packet table + fixups

--- h5pt_test.go
+++ h5pt_test.go
@@ -10,30 +10,39 @@ const (
 	NRECORDS   int    = 8
 )
 
-type particle_t struct {
-	name        string  //"Name"
-	lati        int     //"Latitude"
-	longi       int     //"Longitude"
-	pressure    float32 //"Pressure"
-	temperature float64 //"Temperature"
-	isthep      []int
-	jmohep      [2][2]int
+type particle struct {
+	// name        string  `hdf5:"Name"`     // FIXME(sbinet)
+	lati        int     `hdf5:"Latitude"`
+	longi       int     `hdf5:"Longitude"`
+	pressure    float32 `hdf5:"Pressure"`
+	temperature float64 `hdf5:"Temperature"`
+	// isthep      []int                     // FIXME(sbinet)
+	// jmohep      [2][2]int                 // FIXME(sbinet)
 }
 
 func TestTable(t *testing.T) {
-	t.Skip()
 
 	// define an array of particles
-	p_data := []particle_t{
-		{"zero", 0, 0, 0.0, 0., []int{0, 0}, [2][2]int{{0, 0}, {0, 0}}},
-		{"one", 10, 10, 1.0, 10., []int{0, 0}, [2][2]int{{1, 0}, {0, 1}}},
-		{"two", 20, 20, 2.0, 20., []int{0, 0}, [2][2]int{{2, 0}, {0, 2}}},
-		{"three", 30, 30, 3.0, 30., []int{0, 0}, [2][2]int{{3, 0}, {0, 3}}},
-		{"four", 40, 40, 4.0, 40., []int{0, 0}, [2][2]int{{4, 0}, {0, 4}}},
-		{"five", 50, 50, 5.0, 50., []int{0, 0}, [2][2]int{{5, 0}, {0, 5}}},
-		{"six", 60, 60, 6.0, 60., []int{0, 0}, [2][2]int{{6, 0}, {0, 6}}},
-		{"seven", 70, 70, 7.0, 70., []int{0, 0}, [2][2]int{{7, 0}, {0, 7}}},
+	particles := []particle{
+		{0, 0, 0.0, 0.},
+		{10, 10, 1.0, 10.},
+		{20, 20, 2.0, 20.},
+		{30, 30, 3.0, 30.},
+		{40, 40, 4.0, 40.},
+		{50, 50, 5.0, 50.},
+		{60, 60, 6.0, 60.},
+		{70, 70, 7.0, 70.},
 	}
+	// particles := []particle{
+	// 	{"zero", 0, 0, 0.0, 0., []int{0, 0}, [2][2]int{{0, 0}, {0, 0}}},
+	// 	{"one", 10, 10, 1.0, 10., []int{0, 0}, [2][2]int{{1, 0}, {0, 1}}},
+	// 	{"two", 20, 20, 2.0, 20., []int{0, 0}, [2][2]int{{2, 0}, {0, 2}}},
+	// 	{"three", 30, 30, 3.0, 30., []int{0, 0}, [2][2]int{{3, 0}, {0, 3}}},
+	// 	{"four", 40, 40, 4.0, 40., []int{0, 0}, [2][2]int{{4, 0}, {0, 4}}},
+	// 	{"five", 50, 50, 5.0, 50., []int{0, 0}, [2][2]int{{5, 0}, {0, 5}}},
+	// 	{"six", 60, 60, 6.0, 60., []int{0, 0}, [2][2]int{{6, 0}, {0, 6}}},
+	// 	{"seven", 70, 70, 7.0, 70., []int{0, 0}, [2][2]int{{7, 0}, {0, 7}}},
+	// }
 
 	chunk_size := 10
 	compress := 0
@@ -58,12 +67,13 @@ func TestTable(t *testing.T) {
 	}
 
 	// write one packet to the packet table
-	if err = table.Append(p_data[0]); err != nil {
+	if err = table.Append(&particles[0]); err != nil {
 		t.Fatalf("Append failed with single packet: %s", err)
 	}
 
 	// write several packets
-	if err = table.Append(p_data[1:]); err != nil {
+	parts := particles[1:]
+	if err = table.Append(&parts); err != nil {
 		t.Fatalf("Append failed with multiple packets: %s", err)
 	}
 
@@ -72,13 +82,13 @@ func TestTable(t *testing.T) {
 	if err != nil {
 		t.Fatalf("NumPackets failed: %s", err)
 	}
-	if n != NRECORDS {
-		t.Fatalf("Wrong number of packets reported, expected %d but got %d", NRECORDS, n)
+	if n != nrecords {
+		t.Fatalf("Wrong number of packets reported, expected %d but got %d", nrecords, n)
 	}
 
 	// iterate through packets
 	for i := 0; i != n; i++ {
-		p := make([]particle_t, 1)
+		p := make([]particle, 1)
 		if err := table.Next(p); err != nil {
 			t.Fatalf("Next failed: %s", err)
 		}
@@ -86,8 +96,12 @@ func TestTable(t *testing.T) {
 
 	// reset index
 	table.CreateIndex()
-	dst_buf := make([]particle_t, NRECORDS)
-	if err = table.ReadPackets(0, NRECORDS, dst_buf); err != nil {
+	parts = make([]particle, nrecords)
+	if err = table.ReadPackets(0, nrecords, &parts); err != nil {
 		t.Fatalf("ReadPackets failed: %s", err)
+	}
+
+	if !reflect.DeepEqual(parts, particles) {
+		t.Fatalf("particles differ.\ngot= %#v\nwant=%#v\n", parts, particles)
 	}
 }
