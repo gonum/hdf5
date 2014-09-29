d1618a57cd0e4c4185fb8b0b40117ec4bd4e4b7b
Simon Leblanc <sim.leblanc@gmail.com>
2014-09-29 18:41:40 -0400

    Move tiny C file to go file

--- errors.go
+++ errors.go
@@ -1,9 +1,14 @@
 package hdf5
 
 // #include "hdf5.h"
-// #include <stdlib.h>
-// #include <string.h>
-// #include "cgo_hdf5.h"
+//
+// herr_t _go_hdf5_unsilence_errors(void) {
+//   return H5Eset_auto2(H5E_DEFAULT, (H5E_auto2_t)(H5Eprint), stderr);
+// }
+//
+// herr_t _go_hdf5_silence_errors(void) {
+//   return H5Eset_auto2(H5E_DEFAULT, NULL, NULL);
+// }
 import "C"
 
 import (
