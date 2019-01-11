20752de5676e343a1041ed932b5ad8b7d98531d5
happydig <happydig@163.com>
2019-01-11 16:58:17 +0800

    update cflags and ldflags to support raspbian

--- cgoflags.go
+++ cgoflags.go
@@ -7,7 +7,9 @@ package hdf5
 // #cgo LDFLAGS: -lhdf5 -lhdf5_hl
 // #cgo darwin CFLAGS: -I/usr/local/include
 // #cgo darwin LDFLAGS: -L/usr/local/lib
-// #cgo linux CFLAGS: -I/usr/local/include, -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include
-// #cgo linux LDFLAGS: -L/usr/local/lib, -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
+// #cgo linux !arm CFLAGS: -I/usr/local/include, -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include
+// #cgo linux !arm LDFLAGS: -L/usr/local/lib, -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
+// #cgo linux arm CFLAGS: -I/usr/local/include, -I/usr/lib/arm-linux-gnueabihf/hdf5/serial/include/
+// #cgo linux arm LDFLAGS: -L/usr/local/lib, -L/usr/lib/arm-linux-gnueabihf/hdf5/serial/
 // #include "hdf5.h"
 import "C"
