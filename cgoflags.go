3f5710cad6c0b99d7b4780a4175543d4c9db4051
happydig <37877812+happydig@users.noreply.github.com>
2019-01-11 11:53:36 +0800

    use 'cgo pkg-config' directive

--- cgoflags.go
+++ cgoflags.go
@@ -4,10 +4,7 @@
 
 package hdf5
 
-// #cgo LDFLAGS: -lhdf5 -lhdf5_hl
-// #cgo darwin CFLAGS: -I/usr/local/include
-// #cgo darwin LDFLAGS: -L/usr/local/lib
-// #cgo linux CFLAGS: -I/usr/local/include, -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include
-// #cgo linux LDFLAGS: -L/usr/local/lib, -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
+// #cgo pkg-config: hdf5
+// #cgo LDFLAGS: -lhdf5_hl
 // #include "hdf5.h"
 import "C"
