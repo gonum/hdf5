5025f67107da7923f7d71523a26a8c181ecd453f
yuanqj <yuanqj8191@outlook.com>
2019-03-26 10:50:37 +0800

    Support dataset compression.

--- h5p_proplist.go
+++ h5p_proplist.go
@@ -9,7 +9,12 @@ package hdf5
 // #include <string.h>
 // inline static
 // hid_t _go_hdf5_H5P_DEFAULT() { return H5P_DEFAULT; }
+// hid_t _go_hdf5_H5P_DATASET_CREATE() { return H5P_DATASET_CREATE; }
+// hid_t _go_hdf5_H5_SZIP_EC_OPTION_MASK() { return H5_SZIP_EC_OPTION_MASK; }
+// hid_t _go_hdf5_H5_SZIP_NN_OPTION_MASK() { return H5_SZIP_NN_OPTION_MASK; }
 import "C"
+
+import "unsafe"
 
 type PropType C.hid_t
 
@@ -18,7 +23,10 @@ type PropList struct {
 }
 
 var (
-	P_DEFAULT *PropList = newPropList(C._go_hdf5_H5P_DEFAULT())
+	P_DEFAULT              *PropList = newPropList(C._go_hdf5_H5P_DEFAULT())
+	H5P_DATASET_CREATE     PropType  = PropType(C._go_hdf5_H5P_DATASET_CREATE()) // Properties for dataset creation
+	H5_SZIP_EC_OPTION_MASK           = uint(C._go_hdf5_H5_SZIP_EC_OPTION_MASK()) // Selects entropy coding method.
+	H5_SZIP_NN_OPTION_MASK           = uint(C._go_hdf5_H5_SZIP_NN_OPTION_MASK()) // Selects nearest neighbor coding method.
 )
 
 func newPropList(id C.hid_t) *PropList {
@@ -38,6 +46,35 @@ func NewPropList(cls_id PropType) (*PropList, error) {
 // Close terminates access to a PropList.
 func (p *PropList) Close() error {
 	return p.closeWith(h5pclose)
+}
+
+// Sets the size of the chunks used to store a chunked layout dataset.
+// https://support.hdfgroup.org/HDF5/doc/RM/RM_H5P.html#Property-SetChunk
+func (p *PropList) SetChunk(ndims uint, dim []uint) error {
+	var c_dim *C.hsize_t
+	c_dim = (*C.hsize_t)(unsafe.Pointer(&dim[0]))
+	if err := h5err(C.H5Pset_chunk(C.hid_t(p.id), C.int(ndims), c_dim)); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Sets deflate (GNU gzip) compression method and compression level.
+// https://support.hdfgroup.org/HDF5/doc/RM/RM_H5P.html#Property-SetDeflate
+func (p *PropList) SetDeflate(level uint) error {
+	if err := h5err(C.H5Pset_deflate(C.hid_t(p.id), C.uint(level))); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Sets up use of the SZIP compression filter.
+// https://support.hdfgroup.org/HDF5/doc/RM/RM_H5P.html#Property-SetSzip
+func (p *PropList) SetSzip(optionsMask, pixelsPerBlock uint) error {
+	if err := h5err(C.H5Pset_szip(C.hid_t(p.id), C.uint(optionsMask), C.uint(pixelsPerBlock))); err != nil {
+		return err
+	}
+	return nil
 }
 
 func h5pclose(id C.hid_t) C.herr_t {
---
+++ h5p_proplist_test.go
@@ -0,0 +1,154 @@
+// Copyright ©2017 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package hdf5
+
+import (
+	"errors"
+	"math"
+	"os"
+	"testing"
+)
+
+/**
+ * These test cases are based on the h5_cmprss.c by The HDF Group.
+ * https://support.hdfgroup.org/HDF5/examples/intro.html#c
+ */
+
+func TestDeflate(t *testing.T) {
+	DisplayErrors(true)
+	fn, dsn, dims := "cmprss_deflate.h5", "dset_cmpress", []uint{1000, 1000}
+	defer DisplayErrors(false)
+	defer os.Remove(fn)
+
+	dclp, err := NewPropList(H5P_DATASET_CREATE)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dclp.Close()
+	err = dclp.SetChunk(2, []uint{100, 100})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = dclp.SetDeflate(7)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data0, err := save(fn, dsn, dims, dclp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data1, err := load(fn, dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := compare(data0, data1); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSZip(t *testing.T) {
+	DisplayErrors(true)
+	fn, dsn, dims := "cmprss_szip.h5", "dset_cmpress", []uint{100, 100}
+	defer DisplayErrors(false)
+	defer os.Remove(fn)
+
+	dclp, err := NewPropList(H5P_DATASET_CREATE)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dclp.Close()
+	err = dclp.SetChunk(2, []uint{20, 20})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = dclp.SetSzip(H5_SZIP_EC_OPTION_MASK, 32)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data0, err := save(fn, dsn, dims, dclp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data1, err := load(fn, dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := compare(data0, data1); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func save(fn, dsn string, dims []uint, dclp *PropList) ([]float64, error) {
+	f, err := CreateFile(fn, F_ACC_TRUNC)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	dspace, err := CreateSimpleDataspace(dims, dims)
+	if err != nil {
+		return nil, err
+	}
+
+	dset, err := f.CreateDatasetWith(dsn, T_NATIVE_DOUBLE, dspace, dclp)
+	if err != nil {
+		return nil, err
+	}
+	defer dset.Close()
+
+	n := dims[0] * dims[1]
+	data := make([]float64, n)
+	for i := uint(0); i < n; i++ {
+		data[i] = float64((i*i*i + 13) % 8191)
+	}
+	err = dset.Write(&data[0])
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func load(fn, dsn string) ([]float64, error) {
+	f, err := OpenFile(fn, F_ACC_RDONLY)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	dset, _ := f.OpenDataset(dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer dset.Close()
+
+	dims, _, err := dset.Space().SimpleExtentDims()
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]float64, dims[0]*dims[1])
+	dset.Read(&data[0])
+	return data, nil
+}
+
+func compare(ds0, ds1 []float64) error {
+	n0, n1 := len(ds0), len(ds1)
+	if n0 != n1 {
+		return errors.New("Dimensions not meet")
+	}
+	for i := 0; i < n0; i++ {
+		d := math.Abs(ds0[i] - ds1[i])
+		if d > 1e-7 {
+			return errors.New("Values not meet")
+		}
+	}
+	return nil
+}
