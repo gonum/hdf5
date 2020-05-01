936db714c2e8f7cce20f1dcd3d70233da102227b
Jon Richards <jon.richards@sintelasystems.com>
2020-05-01 17:55:20 +0100

    Adds support for aarch64

--- cgoflags.go
+++ cgoflags.go
@@ -7,7 +7,9 @@ package hdf5
 // #cgo LDFLAGS: -lhdf5 -lhdf5_hl
 // #cgo darwin CFLAGS: -I/usr/local/include
 // #cgo darwin LDFLAGS: -L/usr/local/lib
-// #cgo linux CFLAGS: -I/usr/local/include, -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include
-// #cgo linux LDFLAGS: -L/usr/local/lib, -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
+// #cgo linux,!arm64 CFLAGS: -I/usr/local/include, -I/usr/lib/x86_64-linux-gnu/hdf5/serial/include
+// #cgo linux,!arm64 LDFLAGS: -L/usr/local/lib, -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
+// #cgo linux,arm64 CFLAGS: -I/usr/local/include, -I/usr/lib/aarch64-linux-gnu/hdf5/serial/include
+// #cgo linux,arm64 LDFLAGS: -L/usr/local/lib, -L/usr/lib/aarch64-linux-gnu/hdf5/serial/
 // #include "hdf5.h"
 import "C"
